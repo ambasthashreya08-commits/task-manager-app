4dd65fc3d46a0bb83987547be11fb72bdcc9d11c
ambasthashreya08-commits <ambasthashreya.08@gmail.com>
2026-05-06 18:55:25 +0530

    fixed backend server

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,33 @@
-require("dotenv").config();
-
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
-const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://task-manager-app-ecru-tau.vercel.app",
-    credentials: true,
-  })
-);
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/tasks", taskRoutes);
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-  console.log("MongoDB Connected");
-})
-.catch((err) => {
-  console.log(err);
-});
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
