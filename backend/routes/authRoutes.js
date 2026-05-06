ffbf47425c0a8e10a286d03b79911641270c76d4
ambasthashreya08-commits <ambasthashreya.08@gmail.com>
2026-05-06 16:41:37 +0530

    fixed case sensitivity

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-const User = require("../models/User");
+const user = require("../models/user");
 
 
 // REGISTER
